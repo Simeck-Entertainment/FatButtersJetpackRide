f6b5e9106612a14694108e6b7d9578b857681930
GastricBypass <dsp45@case.edu>
2025-12-24 13:49:21 -0800

    Fixed issues with the settings menu in the level select scene. Added new hideable view models, buttons, and a LevelSelectUIModel to go with them.

---
+++ Assets/Scripts/UI/LevelSelectUI/ViewModels/LevelSelectResumeButtonViewModel.cs
@@ -0,0 +1,7 @@
+﻿public class LevelSelectResumeButtonViewModel : ButtonViewModel<LevelSelectUIModel>
+{
+    protected override void OnClick()
+    {
+        Model.UIState = LevelSelectUIState.Base;
+    }
+}
