f16d1ae404432423e8a819aee6ae17d5fad59c9d
GastricBypass <dsp45@case.edu>
2026-01-06 09:43:48 -0800

    WIP MVVM shop menu

---
+++ Assets/Scripts/UI/ShopMenu/View Models/ShowWhenSkinOwnedViewModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ShowWhenSkinOwnedViewModel : HideableViewModel<ShopMenuModel>
+{
+    [SerializeField] private bool showWhenSkinOwned = true;
+
+    protected override bool IsVisible()
+    {
+        return Model.IsActiveSkinOwned() == showWhenSkinOwned;
+    }
+}
